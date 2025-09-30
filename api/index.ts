07b88e5e5a519f16c089002dce2abbac5900d8f7
lc <lichon@foxmail.com>
2025-09-30 20:13:23 +0800

    realtime api test

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,10 @@
 import { Hono } from 'hono';
-import { createClient } from '@supabase/supabase-js'
 
 const app = new Hono();
 
-app.all('/api', (c) => c.text(`hello ${c.req.path}`));
+app.all('/api', (c) => c.text(`hello ${process.env.SUPABASE_URL}`));
 
-app.get('/api/rooms/:name/message', async (c) => {
-  return c.text(`hello ${c.req.param('name')} ${process.env.SUPABASE_URL}`)
-}).post('/api/rooms/:name/message', async (c) => {
+app.post('/api/rooms/:name/message', async (c) => {
   const name = c.req.param('name')
   if (!name?.length) {
     return c.text('invalid request', 400)
@@ -24,12 +21,12 @@ app.get('/api/rooms/:name/message', async (c) => {
       private: false,
       messages: [
         {
-          topic: "room:asa123:messages",
-          event: "message",
+          topic: `room:${name}:messages`,
+          event: 'message',
           payload: {
             id: crypto.randomUUID(),
             content: body.content,
-            sender: "nickname",
+            sender: 'vercel',
             timestamp: new Date().toISOString()
           }
         }
